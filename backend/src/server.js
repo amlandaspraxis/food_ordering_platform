d12d470eed0f0758f5dd65009fc2e69214500809
Amlan Das <amlan.das.praxis@gmail.com>
2026-03-15 02:00:25 +0530

    fix cors connection

--- backend/src/server.js
+++ backend/src/server.js
@@ -55,7 +55,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use((0, helmet_1.default)());
 app.use((0, cors_1.default)({
-    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+    origin: process.env.FRONTEND_URL || 'https://food-ordering-platform-xi.vercel.app/',
     credentials: true,
 }));
 app.use((0, morgan_1.default)('dev'));
