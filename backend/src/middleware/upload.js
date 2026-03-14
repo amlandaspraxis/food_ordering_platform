a8e5e0b2233377196ac4ab325ce5b798f36c21e4
Amlan Das <amlan.das.praxis@gmail.com>
2026-03-15 02:26:00 +0530

    fix errors

--- backend/src/middleware/upload.js
+++ backend/src/middleware/upload.js
@@ -1,17 +1,20 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.upload = void 0;
-const multer_1 = __importDefault(require("multer"));
+
+const multer = require("multer");
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
-const cloudinary_1 = __importDefault(require("../config/cloudinary"));
+const cloudinary = require("../config/cloudinary");
+
 const storage = new CloudinaryStorage({
-    cloudinary: cloudinary_1.default,
-    params: {
-        folder: 'food-delivery',
-        allowed_formats: ['jpg', 'png', 'jpeg'],
-    },
+  cloudinary: cloudinary,
+  params: async (req, file) => {
+    return {
+      folder: "food-delivery",
+      format: file.mimetype.split("/")[1],
+      public_id: Date.now() + "-" + file.originalname,
+    };
+  },
 });
-exports.upload = (0, multer_1.default)({ storage: storage });
+
+const upload = multer({ storage });
+
+module.exports = { upload };
